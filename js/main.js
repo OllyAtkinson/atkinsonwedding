b489795666fd64090c8271b6d344998fe31aab8d
OllyAtkinson <ollyatkinson96@gmail.com>
2025-01-24 22:56:56 +0000

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-(function ($) {
+/*(function ($) {
     "use strict";
 
     // Navbar on scrolling
@@ -25,7 +25,42 @@
                 $(this).closest('a').addClass('active');
             }
         }
+    }); */
+
+(function ($) {
+    "use strict";
+
+    // Navbar on scrolling
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 200) {
+            $('.navbar').fadeIn('slow').css('display', 'flex');
+        } else {
+            $('.navbar').fadeOut('slow').css('display', 'none');
+        }
+
+        // Automatically close the navbar if it is open and the user starts scrolling
+        if ($('.navbar-toggler').attr('aria-expanded') === "true") {
+            $('.navbar-toggler').click(); // Simulates a click to close the menu
+        }
     });
+
+    // Smooth scrolling on the navbar links
+    $(".navbar-nav a").on('click', function (event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+
+            $('html, body').animate({
+                scrollTop: $(this.hash).offset().top - 45
+            }, 1500, 'easeInOutExpo');
+
+            if ($(this).parents('.navbar-nav').length) {
+                $('.navbar-nav .active').removeClass('active');
+                $(this).closest('a').addClass('active');
+            }
+        }
+    });
+
+
 
 
     // Modal Video
